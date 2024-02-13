ffc1564462a84c46d59302618ef6fc93524d0361
CharlesCCA <142326442+CharlesCCA@users.noreply.github.com>
2024-02-13 15:52:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const header = 'Buttons!'
+
+const para1 = 'Let\'s use buttons to change things!'
+
+// Update header text
+document.querySelector('.header').innerHTML = header
+
+// Update paragraph text
+document.querySelector('.para1').innerHTML = para1
+
+document.querySelector(".deleteText").addEventListener("click", function() {                                 
+  document.querySelector(".para1").innerHTML="";
+})
+
+document.querySelector(".undeleteText").addEventListener("click", function() {                                     
+  document.querySelector(".para1").innerHTML = para1;
+})
+
+document.querySelector(".blueBGColor").addEventListener("click", function() {
+  document.querySelector("body").style.backgroundColor = "darkblue";
+})
+
+document.querySelector(".maroonBGColor").addEventListener("click", function() {
+  document.querySelector("body").style.backgroundColor = "maroon";
+})
+
+document.querySelector(".greenBGColor").addEventListener("click", function() {
+  document.querySelector("body").style.backgroundColor = "darkgreen";
+})
+
+// Log to console
+console.log(message)
